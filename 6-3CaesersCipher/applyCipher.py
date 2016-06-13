11dc46b819f0bef3b81fd4b504ffadf6bbf643c3
josenavarro-leadps <rc.navarro.jose@leadps.org>
2016-06-12 23:21:49 -0700

    this is my cipher code

--- 6-3CaesersCipher/applyCipher.py
+++ 6-3CaesersCipher/applyCipher.py
@@ -1,3 +1,5 @@
+import string
+
 # applyCipher.py
 # A proramm to encrypt/decrypt user text
 # using Caesar's Cipher
@@ -8,36 +10,53 @@
 # arguments: key
 # returns: dictionary of mapped letters
 def createDictionary(key):
-
-
-	#placeholder
-	return {}
+	ALPHABET = string.ascii_uppercase
+	alphabet = string.ascii_lowercase
+	dictionary = {}
+	for x in range(0, len(alphabet)):
+		dictionary[ALPHABET[(x + key) % 26]] = ALPHABET[x]
+	for x in range(0, len(alphabet)):
+		dictionary[alphabet[(x + key) % 26]] = alphabet[x]
+	for x in range(32, 64):
+		dictionary[chr(x)] = chr(x)
+	return dictionary
 
 # the encoded messag from user 
 def getMessage():
-	pass	
+	print("Welcome, what message would you wat to encode?")
+	message = raw_input()
+	return message
 
-# for each letter in message, decodes and records 
-# arguements: encoded messae, dictionary
-#returns: decoded message
-def decodeMessage(message, dictionary):
-	pass
+
+def decodeMessage(message, Dictionary):
+	theMessage = ""
+	for y in message:
+		theMessage = theMessage + Dictionary[y]
+	return theMessage
 
 # outputs the message to the terminal
 # arguements: decoded message
 # returns: none
 def printMessage(message):
-	pass
+	print(message)
 
 
-# 4execution starts here
+# execution starts here
 
 # ask user for key
-print("What key would you like to use to decode?")
-key = int(raw_input())
+try:
+	print("What key do you want to use")
+	key = int(raw_input())
 
-dictionary = createDictionary(key)
-encodedMessge = getMessage()
-decodedMessage = decodeMessage(encodedMessage, dictionary)
-printMessage(decodedMessage)
+	Dictionary = createDictionary(key)
+	encodedMessge = getMessage()
+	decodedMessage = decodeMessage(encodedMessage, dictionary)
+	print("Here is the message coded")
+	printMessage(decodedMessage)
+
+except:
+	print("Sorry, seems like you did a mistake")
+
+
+
 
